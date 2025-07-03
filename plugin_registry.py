ae8b9ce9958e8b0662c3b2bc1d1fe55df1f99cc5
MasterPhooey <106418429+MasterPhooey@users.noreply.github.com>
2025-07-03 09:13:06 -0500

    4.1

--- plugin_registry.py
+++ plugin_registry.py
@@ -5,6 +5,7 @@ from plugins.comfyui_plugin import plugin as comfyui_plugin
 from plugins.comfyui_audio_plugin import plugin as comfyui_audio_plugin
 from plugins.comfyui_video_plugin import plugin as comfyui_video_plugin
 from plugins.comfyui_image_video_plugin import plugin as comfyui_image_video_plugin
+from plugins.comfyui_audio_ace import plugin as comfyui_audio_ace_plugin
 from plugins.automatic_plugin import plugin as automatic_plugin
 from plugins.premiumize_download import plugin as premiumize_download_plugin
 from plugins.premiumize_torrent import plugin as premiumize_torrent_plugin
@@ -28,6 +29,7 @@ plugin_registry = {
     comfyui_audio_plugin.name: comfyui_audio_plugin,
     comfyui_video_plugin.name: comfyui_video_plugin,
     comfyui_image_video_plugin.name: comfyui_image_video_plugin,
+    comfyui_audio_ace_plugin.name: comfyui_audio_ace_plugin,
     automatic_plugin.name: automatic_plugin,
     sftpgo_account_plugin.name: sftpgo_account_plugin,
     premiumize_download_plugin.name: premiumize_download_plugin,
